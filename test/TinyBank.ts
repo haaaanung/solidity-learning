c9f2ae4d9b56a89bd5c9e72ee406cc52b7dbb814
haaaanung <hanung510@naver.com>
2025-05-12 19:18:23 +0900

    tiny bank service

---
+++ test/TinyBank.ts
@@ -0,0 +1,50 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { MyToken, TinyBank } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { any } from "hardhat/internal/core/params/argumentTypes";
+
+const MINTING_AMOUNT = 100n;
+const DECIMALS = 18n;
+
+describe("TinyBank", () => {
+  let signers: HardhatEthersSigner[];
+  let myTokenC: MyToken;
+  let tinyBankC: TinyBank;
+  beforeEach("TinyBank", async () => {
+    signers = await hre.ethers.getSigners();
+    myTokenC = await hre.ethers.deployContract("MyToken", [
+      "MyToken",
+      "MT",
+      DECIMALS,
+      MINTING_AMOUNT,
+    ]);
+    tinyBankC = await hre.ethers.deployContract("TinyBank", [
+      await myTokenC.getAddress(),
+    ]);
+  });
+
+  describe("Initialized state check", () => {
+    it("should return totalStaked 0", async () => {
+      expect(await tinyBankC.totalStaked()).equal(0);
+    });
+    it("should return staked 0 amount of signer0", async () => {
+      const signer0 = signers[0];
+      expect(await tinyBankC.staked(signer0.address)).equal(0);
+    });
+  });
+
+  describe("Staking", async () => {
+    it("should return staked amount", async () => {
+      const signer0 = signers[0];
+      const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+      await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
+      await tinyBankC.stake(stakingAmount);
+      expect(await tinyBankC.staked(signer0.address)).equal(stakingAmount);
+      expect(await tinyBankC.totalStaked()).equal(stakingAmount);
+      expect(await myTokenC.balanceOf(tinyBankC)).equal(
+        await tinyBankC.totalStaked()
+      );
+    });
+  });
+});
